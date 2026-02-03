0755666eddd82fc70e2364c6f4a33f3c09644cbd
gianxzx <tulanagian6@gmail.com>
2026-02-03 23:23:40 +0800

    Update embeds.js

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -1,1 +1,10 @@
+import { EmbedBuilder } from "discord.js";
 
+// Common embed builders
+export const createOrderEmbed = (title, description, color = "Blue") => {
+  return new EmbedBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setColor(color)
+    .setTimestamp();
+};
