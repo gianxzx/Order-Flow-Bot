09a57a457e0e51ad2d978946310cad55c585d221
gianxzx <tulanagian6@gmail.com>
2026-02-03 23:22:46 +0800

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,1 +1,9 @@
+import { client } from "../bot.js";
+import { initClaimHandler } from "../handlers/claimHandler.js";
+import { initDiscountHandler } from "../handlers/discountHandler.js";
 
+// Initialize handlers that rely on interactions (buttons / slash commands)
+export const initInteractionCreate = () => {
+  initClaimHandler();
+  initDiscountHandler();
+};
