9aaffbf335b018bbe1a32478295c87f50d476678
gianxzx <tulanagian6@gmail.com>
2026-02-03 23:22:25 +0800

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,1 +1,8 @@
+import { initCloseHandler } from "../handlers/closeHandler.js";
+import { initWebhookHandler } from "../handlers/webhookHandler.js";
 
+// Initialize handlers that rely on messageCreate
+export const initMessageCreate = () => {
+  initCloseHandler();
+  initWebhookHandler();
+};
