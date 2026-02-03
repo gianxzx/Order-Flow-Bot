5061a5387258c8f944aa898b4af795f48f92e281
gianxzx <tulanagian6@gmail.com>
2026-02-03 23:12:59 +0800

    Update supabase.js

--- src/database/supabase.js
+++ src/database/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { ENV } from "../config/env.js";
 
+// Initialize Supabase client using env variables
 export const supabase = createClient(
   ENV.SUPABASE_URL,
   ENV.SUPABASE_ANON_KEY
